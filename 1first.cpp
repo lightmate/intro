b89f2f0bf40891272cc127527449d028d78c9c7b
Rahul Kumar <lightfreemate@gmail.com>
2022-01-07 18:10:08 +0530

    First commit

--- 1first.cpp
+++ 1first.cpp
@@ -2,17 +2,6 @@
 using namespace std;
 
 int main(){
-    cout<<"Helloooooooooo\n";
-    // taking inputss
-
-    int num1;
-    cin>> num1;
- 
-    int num2;
-    cin>> num2;
-
-    int sum = num1 + num2;
-    cout<<"Printing sum\n";
-    cout<< sum;
+    cout<<"I just changed it "<<endl;
     return 0;
 }
